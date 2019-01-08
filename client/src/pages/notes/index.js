d38df9b0d9f55bbb8da9205dba6ddf0857573570
Oles Odynets <olchyk89@gmail.com>
2019-01-08 17:40:13 +0100

    added default notes template

--- client/src/pages/notes/index.js
+++ client/src/pages/notes/index.js
@@ -1,20 +1,56 @@
 import React, { Component, Fragment } from 'react';
 import './main.css';
 
+const image = "https://yt3.ggpht.com/-qkiz6poo350/AAAAAAAAAAI/AAAAAAAAAAA/tKffXgsObqs/s48-c-k-no-mo-rj-c0xffffff/photo.jpg";
+
+class NoteContributor extends Component {
+	render() {
+		return(
+			<div className="rn_notes-item__default-edtiro-item">
+				<img alt="contributor" src={ image } />
+			</div>
+		);
+	}
+}
+
 class Note extends Component {
 	render() {
 		return(
-			<div className="rn-notes-item void">
+			<div className={ `rn-notes-item ${ (!this.props.isVoid) ? "default" : "void" }` }> {/* lowcontent, mediumcontent, highcontent, fullcontent */}
 				{
 					(!this.props.isVoid) ? (
-						null
+						<Fragment>
+							<div className="rn-notes-item__default-section">
+								<h3 className="rn-notes-item-title">Building your first platform</h3>
+								<p className="rn-notes-item-desc">
+									Add new note to your notes section where you can add description,
+									edit it with your friends.
+								</p>
+							</div>
+							<div className="rn-notes-item__default-progress">
+								<span className="rn-notes-item__default-progress-title">Progress</span>
+								<div className="rn-notes-item__default-progress-bar">
+									<div className="rn-notes-item__default-progress-bar-item first" />
+									<div className="rn-notes-item__default-progress-bar-item second" />
+									<div className="rn-notes-item__default-progress-bar-item third" />
+									<div className="rn-notes-item__default-progress-bar-item fourth" />
+								</div>
+							</div>
+							<div className="rn_notes-item__default-edtiro">
+								<div>
+									<NoteContributor />
+									<NoteContributor />
+									<NoteContributor />
+								</div>
+							</div>
+						</Fragment>
 					) : (
 						<Fragment>
 							<div className="rn-notes-item__void-plusicon">
 								<i className="fas fa-plus" />
 							</div>
-							<h3 className="rn-notes-item__void-title">Add new note</h3>
-							<p className="rn-notes-item__void-desc">
+							<h3 className="rn-notes-item-title">Add new note</h3>
+							<p className="rn-notes-item-desc">
 								Add new note to your notes section where you can add description,
 								edit it with your friends.
 							</p>
@@ -30,9 +66,14 @@ class App extends Component {
 	render() {
 		return(
 			<div className="rn rn-notes">
-				<Note
-					isVoid={ true }
-				/>
+				<div className="rn-notes-grid">
+					<Note
+						isVoid={ true }
+					/>
+					<Note
+						isVoid={ false }
+					/>
+				</div>
 			</div>
 		);
 	}
